ef79ca67aa18b862abeb315ed4b90bf376a7ead4
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2020-10-28 21:15:14 +0000

    Stage 4

--- pluralsight-designingReactComponents/pages/index.js
+++ pluralsight-designingReactComponents/pages/index.js
@@ -1,28 +1,21 @@
-import Footer from "../src/components/Footer/Footer";
-import Header from "../src/components/header/header";
-import Menu from "../src/components/Menu/Menu";
-import Speakers from "../src/components/Speakers/Speakers";
-import SpeakerSearchBar from "../src/components/SpeakerSearchBar/SpeakerSearchBar";
-
-import SpeakerContext from "../src/components/Speakers/SpeakerContext";
+import Layout from '../src/components/Layout/Layout';
 
 function Page() {
-  const speakers = [
-    { imgSrc: "https://via.placeholder.com/300", name: "First Speaker" },
-    { imgSrc: "https://via.placeholder.com/300", name: "Second Speaker" },
-    { imgSrc: "https://via.placeholder.com/300", name: "Third Speaker" },
-  ];
   return (
-    <div>
-      <Header />
-      <Menu />
-      <SpeakerContext.Provider value={speakers}>
-        <SpeakerSearchBar />
-        <Speakers />
-      </SpeakerContext.Provider>
-      <Footer />
-    </div>
+    <Layout>
+      <div className="text-gray-600">
+        <div className="m-6">
+          <p>
+            Silicon Valley Code Camp is a two-day technical conference in
+            Silicon Valley where developers learn from developers focusing on
+            open source, the latest enterprise-focused technologies, software
+            branding, legal issues around software as well as other topics
+            developers are interested in hearing about such as career building
+            and more. topics developers are interested in hearing about.
+          </p>
+        </div>
+      </div>
+    </Layout>
   );
 }
-
 export default Page;
--- pluralsight-designingReactComponents/src/components/Speakers/Speakers.js
+++ pluralsight-designingReactComponents/src/components/Speakers/Speakers.js
@@ -1,53 +1,114 @@
-import React, { useContext } from "react";
-import withData from "./withData";
-import SpeakersRenderProps from "./SpeakersRenderProps";
-import SpeakerContext from "./SpeakerContext";
+import React, { useState, useEffect, useReducer } from "react";
+import axios from "axios";
 
-/**
- * Higher Order Component
- * @param {Array} param0 Speakers array
- */
-// const Speakers = ({ speakers }) => {
-//   return (
-//     <div>
-//       {speakers.map(({ imgSrc, name }) => {
-//         return <img src={imgSrc} alt={name} key={name} />;
-//       })}
-//     </div>
-//   );
-// };
-// const maxSpeakersToShow = 2;
-// export default withData(maxSpeakersToShow)(Speakers);
+import SpeakerSearchBar from "../SpeakerSearchBar/SpeakerSearchBar";
+import Speaker from "../Speaker/Speaker";
 
-/**
- * Render Props which pass data to their child components.
- */
-// const Speakers = () => {
-//   return (
-//     <SpeakersRenderProps>
-//       {({ speakers }) => {
-//         return (
-//           <div>
-//             {speakers.map(({ imgSrc, name }) => {
-//               return <img src={imgSrc} alt={name} key={name} />;
-//             })}
-//           </div>
-//         );
-//       }}
-//     </SpeakersRenderProps>
-//   );
-// };
-// export default Speakers;
+import requestReducer from "../../reducers/request";
+
+import {
+  GET_ALL_FAILURE,
+  GET_ALL_SUCCESS,
+  PUT_FAILURE,
+  PUT_SUCCESS,
+} from "../../actions/request";
+
+const REQUEST_STATUS = {
+  LOADING: "loading",
+  SUCCESS: "success",
+  ERROR: "error",
+};
 
 const Speakers = () => {
-  const speakers = useContext(SpeakerContext);
+  const onFavoriteToggleHandler = async (speakerRec) => {
+    try {
+      const toggledSpeakerRec = {
+        ...speakerRec,
+        isFavorite: !speakerRec.isFavorite,
+      };
+      await axios.put(
+        `http://localhost:4000/speakers/${speakerRec.id}`,
+        toggledSpeakerRec
+      );
+      dispatch({
+        type: PUT_SUCCESS,
+        record: toggledSpeakerRec,
+      });
+    } catch (e) {
+      dispatch({
+        type: PUT_FAILURE,
+        error: e,
+      });
+    }
+  };
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [{ records: speakers, status, error }, dispatch] = useReducer(
+    requestReducer,
+    {
+      records: [],
+      status: REQUEST_STATUS.LOADING,
+      error: null,
+    }
+  );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/speakers/");
+
+        dispatch({
+          type: GET_ALL_SUCCESS,
+          records: response.data,
+        });
+      } catch (e) {
+        console.log("Loading data error", e);
+        dispatch({
+          type: GET_ALL_FAILURE,
+          error: e,
+        });
+      }
+    };
+    fetchData();
+  }, []);
+
+  const success = status === REQUEST_STATUS.SUCCESS;
+  const isLoading = status === REQUEST_STATUS.LOADING;
+  const hasErrored = status === REQUEST_STATUS.ERROR;
+
   return (
     <div>
-      {speakers.map(({ imgSrc, name }) => {
-        return <img src={imgSrc} alt={name} key={name} />;
-      })}
+      <SpeakerSearchBar
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      {isLoading && <div>Loading...</div>}
+      {hasErrored && (
+        <div>
+          Loading error... Is the json-server running? (try "npm run
+          json-server" at terminal prompt)
+          <br />
+          <b>ERROR: {error.message}</b>
+        </div>
+      )}
+      {success && (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-12">
+          {speakers
+            .filter((rec) => {
+              const targetString = `${rec.firstName} ${rec.lastName}`.toLowerCase();
+              return searchQuery.length === 0
+                ? true
+                : targetString.includes(searchQuery.toLowerCase());
+            })
+            .map((speaker) => (
+              <Speaker
+                key={speaker.id}
+                {...speaker}
+                onFavoriteToggle={() => onFavoriteToggleHandler(speaker)}
+              />
+            ))}
+        </div>
+      )}
     </div>
   );
 };
-
 export default Speakers;
