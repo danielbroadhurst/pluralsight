0a819aa1ccc251b55cfc1868366ad25647349905
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2020-10-31 16:09:28 +0000

    looked at refs

--- pluralsight-managingReactState/src/services/useFetch.js
+++ pluralsight-managingReactState/src/services/useFetch.js
@@ -1,30 +1,35 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect, useRef } from "react";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetch(url) {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const isMounted = useRef(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        async function init() {
-            try {
-                const response = await fetch(baseUrl + url);
-                if (response.ok) {
-                    const json = await response.json();
-                    setData(json);
-                } else {
-                    throw response;
-                }
-            } catch (e) {
-                setError(e)
-            } finally {
-                setLoading(false)
-            }
+  useEffect(() => {
+    isMounted.current = true;
+    async function init() {
+      try {
+        const response = await fetch(baseUrl + url);
+        if (response.ok) {
+          const json = await response.json();
+          if (isMounted.current) setData(json);
+        } else {
+          throw response;
         }
-        init();
-    }, [url]);
+      } catch (e) {
+        if (isMounted.current) setError(e);
+      } finally {
+        if (isMounted.current) setLoading(false);
+      }
+    }
+    init();
+    return () => {
+      isMounted.current = false;
+    };
+  }, [url]);
 
-    return {data, error, loading};
+  return { data, error, loading };
 }
