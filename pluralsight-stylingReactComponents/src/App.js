e848984a80487687677c0af5f5fa63cd77d81899
G2S\daniel.broadhurst <daniel.broadhurst@matchstickmanhub.com>
2020-11-02 12:46:35 +0000

    CSS Modules

--- pluralsight-stylingReactComponents/src/App.js
+++ pluralsight-stylingReactComponents/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { ThemeProvider } from '@friendsofreactjs/react-css-themr'
 
 import './App.css'
-import './NewsletterOverride.css'
 import Newsletter from './Newsletter.js'
+import * as css from './NewsletterOverride.module.css'
+
+const theme = {
+  Newsletter: css
+}
 
 function App() {
   return (
     <div className="app__newsletter">
-      <Newsletter />
+      <ThemeProvider theme={theme}>
+        <Newsletter />
+      </ThemeProvider>
     </div>
   )
 }
--- pluralsight-stylingReactComponents/src/Newsletter.js
+++ pluralsight-stylingReactComponents/src/Newsletter.js
@@ -1,34 +1,37 @@
 import React from 'react'
+import { themr } from '@friendsofreactjs/react-css-themr'
 
-import './Newsletter.css'
+import css from './Newsletter.module.css'
 
 function Newsletter(props) {
   const [email, setEmail] = React.useState('')
   const emailPartsCount = countEmailParts(email)
   return (
-    <section className="es-newsletter">
-      <div className="es-newsletter__spectrum" aria-hidden>
+    <section className={props.theme.container}>
+      <div className={props.theme.spectrum} aria-hidden>
         {Array.from(Array(5)).map((_, i) => (
           <div
-            className={`es-newsletter__bar ${i + 1 <= emailPartsCount &&
-              'es-newsletter__bar--active'}`}
+            className={
+              i + 1 <= emailPartsCount ? props.theme.barActive : props.theme.bar
+            }
             key={i}
           ></div>
         ))}
       </div>
-      <header className="es-newsletter__header">
+      <header className={props.theme.header}>
         <h2>Get the newsletter</h2>
       </header>
       <input
-        className="es-newsletter__email"
+        className={props.theme.email}
         type="email"
         placeholder="Your email"
         value={email}
-        onChange={evt => setEmail(evt.target.value)}
+        onChange={(evt) => setEmail(evt.target.value)}
       />
       <button
-        className={`es-newsletter__submit ${emailPartsCount >= 5 &&
-          'es-newsletter__submit--active'}`}
+        className={
+          emailPartsCount >= 5 ? props.theme.submitActive : props.theme.submit
+        }
       >
         Sign up
       </button>
@@ -36,7 +39,7 @@ function Newsletter(props) {
   )
 }
 
-export default Newsletter
+export default themr('Newsletter', css)(Newsletter)
 
 function countEmailParts(email) {
   if (/@.+\..{2,}$/.test(email)) {
