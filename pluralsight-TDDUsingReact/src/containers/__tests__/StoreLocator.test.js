758b1a517dc08cd1536b9d9e3380ae6c655204a7
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2020-11-07 09:43:55 +0000

    Testing React Components

--- pluralsight-TDDUsingReact/src/containers/__tests__/StoreLocator.test.js
+++ pluralsight-TDDUsingReact/src/containers/__tests__/StoreLocator.test.js
@@ -1,29 +1,59 @@
-import React from "react";
-import { shallow } from "enzyme";
-import StoreLocator from "../StoreLocator";
+import React from 'react';
+import {shallow} from 'enzyme';
+import StoreLocator from '../StoreLocator';
+import axios from 'axios';
 
 describe("StoreLocator", function () {
-  let mountedStoreLocator;
-  beforeEach(() => {
-    mountedStoreLocator = shallow(<StoreLocator />);
-  });
 
-  it("renders without crashing", () => {
-    let mountedStoreLocator = shallow(<StoreLocator />);
-  });
+    let mountedStoreLocator;
+    beforeEach(() => {
+        mountedStoreLocator = shallow(<StoreLocator />);
+    });
 
-  it("renders a header", () => {
-    const headers = mountedStoreLocator.find("Header");
-    expect(headers.length).toBe(1);
-  });
+    it('calls axios.get in #componentDidMount', ()=>{
+        return mountedStoreLocator.instance().componentDidMount().then(()=>{
+            expect(axios.get).toHaveBeenCalled();
+        })
+    });
 
-  it("renders two buttons", () => {
-    const buttons = mountedStoreLocator.find("Button");
-    expect(buttons.length).toBe(2);
-  });
+    it('calls axios.get with correct url', ()=>{
+        return mountedStoreLocator.instance().componentDidMount().then(()=>{
+            expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/data/shops.json');
+        })
+    });
 
-  it("renders a map", () => {
-    const maps = mountedStoreLocator.find("Map");
-    expect(maps.length).toBe(1);
-  });
+    it('updates state with api data', () => {
+        return mountedStoreLocator.instance().componentDidMount().then(()=> {
+          expect(mountedStoreLocator.state()).toHaveProperty('shops',
+              [{
+                  "location": "test location",
+                  "address" : "test address"
+              }]
+          );
+        })
+        });
+
+    it('renders without crashing', () => {
+        let mountedStoreLocator = shallow(<StoreLocator />);
+    });
+
+    it('renders a header', () => {
+        const headers = mountedStoreLocator.find('Header');
+        expect(headers.length).toBe(1);
+    });
+
+
+    it('renders a map', () => {
+        const maps = mountedStoreLocator.find('Map');
+        expect(maps.length).toBe(1);
+    })
+});
+
+describe('chooseMap', ()=> {
+    it('updates this.state.currentMap using the location passed to it', ()=>{
+        let mountedStoreLocator = shallow(<StoreLocator />);
+        let mockEvent = {target:{value:'testland'}};
+        mountedStoreLocator.instance().chooseMap(mockEvent);
+        expect(mountedStoreLocator.instance().state.currentMap).toBe('testland.png');
+    })
 });
