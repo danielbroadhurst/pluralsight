a17d3088affe57aa91e9b7e29c2ebaf9d0697dd6
G2S\daniel.broadhurst <daniel.broadhurst@matchstickmanhub.com>
2020-11-12 10:21:26 +0000

    typed arrays

--- javascript/pluralsight-javascriptArraysCollections/04/js/page.js
+++ javascript/pluralsight-javascriptArraysCollections/04/js/page.js
@@ -1,12 +1,12 @@
 // Accessing the objects
-var ctx = document.getElementById('monthlySales').getContext('2d');
-var pieCtx = document.getElementById('deptSales').getContext('2d');
-var yearlyLabel = document.getElementById('yearlyTotal');
-var newAmount = document.getElementById('itemAmount');
-var newMonth = document.getElementById('monthId');
-let hikingRadio = document.getElementById('hiking');
-let runningRadio = document.getElementById('running');
-let huntingRadio = document.getElementById('hunting');
+var ctx = document.getElementById("monthlySales").getContext("2d");
+var pieCtx = document.getElementById("deptSales").getContext("2d");
+var yearlyLabel = document.getElementById("yearlyTotal");
+var newAmount = document.getElementById("itemAmount");
+var newMonth = document.getElementById("monthId");
+let hikingRadio = document.getElementById("hiking");
+let runningRadio = document.getElementById("running");
+let huntingRadio = document.getElementById("hunting");
 
 // Monthly Totals
 var yearlyTotal = 0;
@@ -14,76 +14,80 @@ var yearlyTotal = 0;
 const monthlySales = new Map();
 
 {
-	let salesA = {
-		a:[1,2]
-	}
+  let salesA = {
+    a: [1, 2],
+  };
 
-	var map = new WeakMap();
-	map.set(salesA, 'Hiking');
- 
-	console.log('First ' + salesA)
+  var map = new WeakMap();
+  map.set(salesA, "Hiking");
+
+  console.log("First " + map);
 }
 
-// console.log('Second ' + salesA);
+//console.log("Second " + map);
 
 // Add Sales
-function addSale(){
-	monthlySales.set(newMonth.value, parseInt(newAmount.value))
-	
-	// Update our labels
-	monthlySalesChart.data.labels = Array.from(monthlySales.keys());
+function addSale() {
+  monthlySales.set(newMonth.value, parseInt(newAmount.value));
 
-	yearlyTotal = 0;
+  // Update our labels
+  monthlySalesChart.data.labels = Array.from(monthlySales.keys());
 
-	monthlySalesChart.data.datasets.forEach((dataset) => {
-	    dataset.data = [];
-	});
+  yearlyTotal = 0;
 
-	for (let amount of monthlySales.values()){
-		yearlyTotal = amount + yearlyTotal;
-		yearlyLabel.innerHTML = yearlyTotal;
+  monthlySalesChart.data.datasets.forEach((dataset) => {
+    dataset.data = [];
+  });
 
-		monthlySalesChart.data.datasets.forEach((dataset) => {
-	        dataset.data.push(amount);
-	    });
-	}
+  for (let amount of monthlySales.values()) {
+    yearlyTotal = amount + yearlyTotal;
+    yearlyLabel.innerHTML = yearlyTotal;
 
-	monthlySalesChart.update();
+    monthlySalesChart.data.datasets.forEach((dataset) => {
+      dataset.data.push(amount);
+    });
+  }
 
+  monthlySalesChart.update();
 }
 
-function findSale(){
-	
+function findSale() {
+  monthlySales.get("key"); // returns value
+  monthlySales.has("key"); // returns boolean
 }
 
-function fillValue(){
-	
+function fillValue() {
+  monthlySales.delete("key"); // deletes key
 }
 // Bar chart
 var monthlySalesChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: [],
-        datasets: [{
-            label: '# of Sales',
-            data: [],
-            backgroundColor: [
-                'rgba(238, 184, 104, 1)',
-                'rgba(75, 166, 223, 1)',
-                'rgba(239, 118, 122, 1)',
-            ],
-            borderWidth: 0
-        }]
+  type: "bar",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "# of Sales",
+        data: [],
+        backgroundColor: [
+          "rgba(238, 184, 104, 1)",
+          "rgba(75, 166, 223, 1)",
+          "rgba(239, 118, 122, 1)",
+        ],
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+  },
 });
 
 // Pie Chart
@@ -103,6 +107,6 @@ var monthlySalesChart = new Chart(ctx, {
 //         }]
 //     },
 //     options: {
-        
+
 //     }
 // })
