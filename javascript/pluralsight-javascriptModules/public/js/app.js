dd5bd1ce2bc3f31881b3d6825fe5b4ac31bac6ae
G2S\daniel.broadhurst <daniel.broadhurst@matchstickmanhub.com>
2020-11-13 16:41:36 +0000

    using modules

--- javascript/pluralsight-javascriptModules/public/js/app.js
+++ javascript/pluralsight-javascriptModules/public/js/app.js
@@ -1,20 +1,19 @@
-(function (){
-  
-  function render() {
-    var list = document.querySelector('#sessions');
-    if (!list) return;
-    list.innerHTML = sessionTemplate(data.listItems);
-  };
-  
-  
-  var data = {
-    listItems: []
-  };
-  
-  getSessions()
-    .then((sessions)=>{
-      console.log('promises!')
-      data.listItems = sessions;
-      render();
-    });
-}())
+import getSessions, * as sessionRepository from './sessionRepository.js';
+
+function render() {
+  var list = document.querySelector('#sessions');
+  if (!list) return;
+  list.innerHTML = sessionRepository.sessionTemplate(data.listItems);
+};
+
+
+var data = {
+  listItems: []
+};
+
+getSessions()
+  .then((sessions) => {
+    console.log('promises!')
+    data.listItems = sessions;
+    render();
+  });
