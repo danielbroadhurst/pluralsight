0133cbc184eb2398ae7c0fcb1658e43d59495247
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2020-11-25 22:16:07 +0000

    async and generator

--- node/pluralsight-asyncPatternNode/server/routes/clothing.js
+++ node/pluralsight-asyncPatternNode/server/routes/clothing.js
@@ -1,35 +1,49 @@
-const express = require('express');
-const fs = require('fs');
-const datafile = 'server/data/clothing.json';
+const express = require("express");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const datafile = "server/data/clothing.json";
 const router = express.Router();
 
 /* GET all clothing */
-router.route('/')
-  .get(function(req, res) {
+router.route("/").get(async function (req, res) {
+  try {
+    let data = await getClothingData();
+    console.log("Returning async data.");
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 
-    getClothingData((err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        console.log('Returning clothing data');
-        res.send(data);
-      }
-    });
+  // CALLBACK
+  // getClothingData()
+  //   .then(data => {
+  //     console.log('Returning clothing data to browser.');
+  //     res.send(data);
+  //   })
+  //   .catch(error => res.status(500).send(error))
+  //   .finally(() => console.log('All done processing promise.'));
+});
 
-    console.log('Doing more work');
-  });
+async function getClothingData() {
+  let rawData = await fsPromises.readFile(datafile, "utf8");
+  let clothingData = JSON.parse(rawData);
 
-function getClothingData(callback) {
-  fs.readFile(datafile, 'utf8', (err, data) => {
-    if (err) {
-      callback(err, null);
-    }
-    else {
-      let clothingData = JSON.parse(data);
-      callback(null, clothingData);
-    }
-  });
-}  
+  console.log(clothingData);
+
+  return clothingData;
+
+  // CALLBACK
+  // return new Promise((resolve, reject) => {
+  //   fs.readFile(datafile, 'utf8', (err, data) => {
+  //     if (err) {
+  //       reject(err);
+  //     }
+  //     else {
+  //       let clothingData = JSON.parse(data);
+  //       resolve(clothingData);
+  //     }
+  //   });
+  // });
+}
 
 module.exports = router;
