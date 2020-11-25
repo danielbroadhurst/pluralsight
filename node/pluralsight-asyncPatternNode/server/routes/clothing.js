ead2f0953e4ca0d6a099ab2b28ec580a97f5bb8a
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2020-11-25 22:36:32 +0000

    using EventEmitters

--- node/pluralsight-asyncPatternNode/server/routes/clothing.js
+++ node/pluralsight-asyncPatternNode/server/routes/clothing.js
@@ -1,49 +1,82 @@
-const express = require("express");
-const fs = require("fs");
-const fsPromises = require("fs").promises;
-const datafile = "server/data/clothing.json";
+const express = require('express');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const datafile = 'server/data/clothing.json';
 const router = express.Router();
 
-/* GET all clothing */
-router.route("/").get(async function (req, res) {
-  try {
-    let data = await getClothingData();
-    console.log("Returning async data.");
-    res.send(data);
-  } catch (error) {
-    res.status(500).send(error);
+module.exports = function(monitor) {
+  let dataMonitor = monitor;
+
+  dataMonitor.on('dataAdded', (item) => {
+    setImmediate(() => console.log(`New data was added: ${item}`));
+  });
+  
+  /* GET all clothing */
+  router.route('/')
+    .get(async function(req, res) {
+  
+      try {
+        let data = await getClothingData();
+        res.send(data);
+      }
+      catch (error) {
+        res.status(500).send(error);
+      }
+    })
+  
+    .post(async function(req, res) {
+      try {
+  
+        let data = await getClothingData();
+  
+        let nextID = getNextAvailableID(data);
+  
+        let newClothingItem = {
+            clothingID: nextID,
+            itemName: req.body.itemName,
+            price: req.body.price
+        };
+  
+        data.push(newClothingItem);
+  
+        await saveClothingData(data);
+  
+        dataMonitor.emit('dataAdded', newClothingItem.itemName);
+
+        console.log('Returning new item to browser.');
+  
+        res.status(201).send(newClothingItem);
+      }
+      catch (error) {
+        res.status(500).send(error);
+      }
+  
+    });
+  
+  async function getClothingData() {
+  
+    let rawData = await fsPromises.readFile(datafile, 'utf8');
+    let clothingData = JSON.parse(rawData);
+  
+    return clothingData;
+  
+  }  
+  
+  function getNextAvailableID(allClothingData) {
+    
+    let maxID = 0;
+  
+    allClothingData.forEach(function(element, index, array) {
+      if(element.clothingID > maxID) {
+          maxID = element.clothingID;
+      }
+    });
+    return ++maxID;
   }
-
-  // CALLBACK
-  // getClothingData()
-  //   .then(data => {
-  //     console.log('Returning clothing data to browser.');
-  //     res.send(data);
-  //   })
-  //   .catch(error => res.status(500).send(error))
-  //   .finally(() => console.log('All done processing promise.'));
-});
-
-async function getClothingData() {
-  let rawData = await fsPromises.readFile(datafile, "utf8");
-  let clothingData = JSON.parse(rawData);
-
-  console.log(clothingData);
-
-  return clothingData;
-
-  // CALLBACK
-  // return new Promise((resolve, reject) => {
-  //   fs.readFile(datafile, 'utf8', (err, data) => {
-  //     if (err) {
-  //       reject(err);
-  //     }
-  //     else {
-  //       let clothingData = JSON.parse(data);
-  //       resolve(clothingData);
-  //     }
-  //   });
-  // });
+  
+  function saveClothingData(data) {
+    return fsPromises.writeFile(datafile, JSON.stringify(data, null, 4));
+  }
+  
+  return router;
 }
-
-module.exports = router;
