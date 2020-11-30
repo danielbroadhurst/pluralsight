be7bff6b4111ebc66e4e5a06ab75b381bc3aa8d5
G2S\daniel.broadhurst <daniel.broadhurst@matchstickmanhub.com>
2020-11-30 08:41:08 +0000

    updated file from github

--- aws/pluralsight-designingDeveloping/scripts/module_02/create-ec2-instance.js
+++ aws/pluralsight-designingDeveloping/scripts/module_02/create-ec2-instance.js
@@ -1,105 +1,105 @@
 // Imports
-const AWS = require("aws-sdk");
-const helpers = require("./helpers");
+const AWS = require('aws-sdk')
+const helpers = require('./helpers')
 
-AWS.config.update({ region: "eu-west-2" });
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
-const ec2 = new AWS.EC2();
-const sgName = "hamster_sg";
-const keyName = "hamster_key";
+const ec2 = new AWS.EC2()
+const sgName = 'hamster_sg2'
+const keyName = 'hamster_key2'
 
 // Do all the things together
 createSecurityGroup(sgName)
-  .then(() => {
-    return createKeyPair(keyName);
-  })
-  .then(helpers.persistKeyPair)
-  .then(() => {
-    return createInstance(sgName, keyName);
-  })
-  .then((data) => {
-    console.log("Created instance with:", data);
-  })
-  .catch((err) => {
-    console.error("Failed to create instance with:", err);
-  });
+.then(() => {
+  return createKeyPair(keyName)
+})
+.then(helpers.persistKeyPair)
+.then(() => {
+  return createInstance(sgName, keyName)
+})
+.then((data) => {
+  console.log('Created instance with:', data)
+})
+.catch((err) => {
+  console.error('Failed to create instance with:', err)
+})
 
 // Create functions
 
-function createSecurityGroup(sgName) {
+function createSecurityGroup (sgName) {
   const params = {
     Description: sgName,
-    GroupName: sgName,
-  };
+    GroupName: sgName
+  }
 
   return new Promise((resolve, reject) => {
     ec2.createSecurityGroup(params, (err, data) => {
-      if (err) reject(err);
+      if (err) reject(err)
       else {
         const params = {
           GroupId: data.GroupId,
           IpPermissions: [
             {
-              IpProtocol: "tcp",
+              IpProtocol: 'tcp',
               FromPort: 22,
               ToPort: 22,
               IpRanges: [
                 {
-                  CidrIp: "0.0.0.0/0",
-                },
-              ],
-            },
-            {
-              IpProtocol: "tcp",
+                  CidrIp: '0.0.0.0/0'
+                }
+              ]
+            }, {
+              IpProtocol: 'tcp',
               FromPort: 3000,
               ToPort: 3000,
               IpRanges: [
                 {
-                  CidrIp: "0.0.0.0/0",
-                },
-              ],
-            },
-          ],
-        };
+                  CidrIp: '0.0.0.0/0'
+                }
+              ]
+            }
+          ]
+        }
         ec2.authorizeSecurityGroupIngress(params, (err) => {
-          if (err) reject(err);
-          else resolve();
-        });
+          if (err) reject(err)
+          else resolve()
+        })
       }
-    });
-  });
+    })
+  })
 }
 
-function createKeyPair(keyName) {
+function createKeyPair (keyName) {
   const params = {
-    KeyName: keyName,
-  };
+    KeyName: keyName
+  }
 
   return new Promise((resolve, reject) => {
     ec2.createKeyPair(params, (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  });
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
 
-function createInstance(sgName, keyName) {
+function createInstance (sgName, keyName) {
   const params = {
-    ImageId: "ami-013120bd4a57d66e2",
-    InstanceType: "t2.micro",
+    ImageId: 'ami-1c49ae66',
+    InstanceType: 't2.micro',
     KeyName: keyName,
     MaxCount: 1,
     MinCount: 1,
-    SecurityGroups: [sgName],
-    UserData:
-      "IyEvYmluL2Jhc2gNCnN1ZG8gYXB0LWdldCB1cGRhdGUNCnN1ZG8gYXB0LWdldCAteSBpbnN0YWxsIGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQgL2hvbWUvYml0bmFtaS9oYmZsDQpjaG93biAtUiBiaXRuYW1pOiAvaG9tZS9iaXRuYW1pL2hiZmwNCmNkIC9ob21lL2JpdG5hbWkvaGJmbA0Kc3VkbyBucG0gaQ0Kc3VkbyBucG0gcnVuIHN0YXJ0",
-  };
+    SecurityGroups: [
+      sgName
+    ],
+    UserData: 'IyEvYmluL2Jhc2gNCnN1ZG8gYXB0LWdldCB1cGRhdGUNCnN1ZG8gYXB0LWdldCAteSBpbnN0YWxsIGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQgL2hvbWUvYml0bmFtaS9oYmZsDQpjaG93biAtUiBiaXRuYW1pOiAvaG9tZS9iaXRuYW1pL2hiZmwNCmNkIC9ob21lL2JpdG5hbWkvaGJmbA0Kc3VkbyBucG0gaQ0Kc3VkbyBucG0gcnVuIHN0YXJ0'
+  }
 
   return new Promise((resolve, reject) => {
     ec2.runInstances(params, (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  });
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
--- aws/pluralsight-designingDeveloping/scripts/module_02/manage-ec2-instance.js
+++ aws/pluralsight-designingDeveloping/scripts/module_02/manage-ec2-instance.js
@@ -1,38 +1,40 @@
 // Imports
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk')
 
-AWS.config.update({ region: "eu-west-2" });
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
-const ec2 = new AWS.EC2();
+const ec2 = new AWS.EC2()
 
-function listInstances() {
+function listInstances () {
   return new Promise((resolve, reject) => {
     ec2.describeInstances({}, (err, data) => {
-      if (err) reject(err);
+      if (err) reject(err)
       else {
-        resolve(
-          data.Reservations.reduce((i, r) => {
-            return i.concat(r.Instances);
-          }, [])
-        );
+        resolve(data.Reservations.reduce((i, r) => {
+          return i.concat(r.Instances)
+        }, []))
       }
-    });
-  });
+    })
+  })
 }
 
-function terminateInstance(instanceId) {
+function terminateInstance (instanceId) {
   const params = {
-    InstanceIds: [instanceId],
-  };
+    InstanceIds: [
+      instanceId
+    ]
+  }
 
   return new Promise((resolve, reject) => {
     ec2.terminateInstances(params, (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  });
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
 
-listInstances().then((data) => console.log(data));
-// terminateInstance("i-0216ce05d3cca1dc0").then((data) => console.log(data));
+listInstances()
+.then(data => console.log(data))
+// terminateInstance('i-0733b5248eeb48563')
+// .then(data => console.log(data))
