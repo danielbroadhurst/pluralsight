02f5d3417ca7b9a0cc0f1b49b2a279bb9ea2385a
G2S\daniel.broadhurst <daniel.broadhurst@matchstickmanhub.com>
2020-11-30 08:59:10 +0000

    scaling and storage

--- aws/pluralsight-designingDeveloping/scripts/module_04/create-ec2-instance.js
+++ aws/pluralsight-designingDeveloping/scripts/module_04/create-ec2-instance.js
@@ -1,13 +1,13 @@
 // Imports
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/* TODO: Add your regions */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 const ec2 = new AWS.EC2()
 const sgName = 'hamster_sg'
 const keyName = 'hamster_key'
-const instanceId = '/* TODO: Add the instance Id to stop */'
+const instanceId = 'i-0e5a2314c79567aef'
 
 stopInstance(instanceId)
 .then(() => createInstance(sgName, keyName))
@@ -15,13 +15,13 @@ stopInstance(instanceId)
 
 function createInstance (sgName, keyName) {
   const params = {
-    ImageId: '/* TODO: Add ami id for aws linux */',
+    ImageId: 'ami-8c1be5f6',
     InstanceType: 't2.micro',
     KeyName: keyName,
     MaxCount: 1,
     MinCount: 1,
     Placement: {
-      AvailabilityZone: '/* TODO: Add the az from the instance that is stopping */'
+      AvailabilityZone: 'us-east-1b'
     },
     SecurityGroups: [
       sgName
