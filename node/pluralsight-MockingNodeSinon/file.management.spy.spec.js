74cf2c902192057f3f67d5e5cfda38e7350d375f
G2S\daniel.broadhurst <daniel.broadhurst@matchstickmanhub.com>
2020-12-01 08:47:18 +0000

    stubbing functions

--- node/pluralsight-MockingNodeSinon/file.management.spy.spec.js
+++ node/pluralsight-MockingNodeSinon/file.management.spy.spec.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const proxyquire = require("proxyquire");
 const fileManagement = require("./file.management");
 
-describe("File Management", () => {
+describe.skip("File Management", () => {
   afterEach(() => {
     sinon.restore();
   });
@@ -24,7 +24,7 @@ describe("File Management", () => {
       } catch (error) {}
       expect(writeSpy.notCalled).to.be.true;
     });
-    it.skip("Should call writeFileSync - injected", () => {
+    it("Should call writeFileSync - injected", () => {
       const writeSpy = spy(fs, "writeFileSync");
       fileManagement.createFileInjected("test.txt", fs);
 
---
+++ node/pluralsight-MockingNodeSinon/file.management.stub.spec.js
@@ -0,0 +1,61 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+const fs = require("fs");
+const proxyquire = require("proxyquire");
+const fileManagement = require("./file.management");
+
+describe("File Management", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+  it("Should write a new file", () => {
+    const writeStub = sinon.stub(fs, "writeFileSync");
+    const fileManagement = proxyquire("./file.management", { fs });
+
+    fileManagement.createFile("test.txt");
+
+    expect(writeStub.callCount).to.equal(1);
+  });
+  it("Should throw an excection if file already exists", () => {
+    const writeStub = sinon.stub(fs, "writeFileSync");
+    writeStub.throws(new Error());
+    const fileManagement = proxyquire("./file.management", { fs });
+
+    expect(() => fileManagement.createFile("test.txt")).to.throw();
+  });
+  it("createFileSafe should create a file named test1 when test already exists", () => {
+    const writeStub = sinon.stub(fs, "writeFileSync");
+    const readStub = sinon.stub(fs, "readdirSync");
+
+    const fileManagement = proxyquire("./file.management", { fs });
+
+    writeStub.withArgs("./data/test.txt").throws(new Error());
+    writeStub.returns(undefined);
+
+    readStub.returns(["test.txt"]);
+    expect(() => fileManagement.createFile("test.txt")).to.throw();
+  });
+  it("getAllFiles should reutrn a list of files", function () {
+    const readStub = sinon.stub(fs, "readdir");
+
+    const fileManagement = proxyquire("./file.management", { fs });
+    readStub.yields(null, ["test.txt"]);
+
+    fileManagement.getAllFiles((err, data) => {
+      expect(data).to.eql(["test.txt"]);
+    });
+  });
+  it("getAllFilesPromise should reutrn a list of files", function () {
+    const readStub = sinon.stub(fs, "readdir");
+
+    const util = {
+      promisify: sinon.stub().returns(readStub),
+    };
+
+    const fileManagement = proxyquire("./file.management", { fs, util });
+    readStub.resolves(["test.txt"]);
+    return fileManagement
+      .getAllFilesPromise()
+      .then((files) => expect(files).to.eql(["test.txt"]));
+  });
+});
