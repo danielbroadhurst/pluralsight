e6cb08fe22a9392cee63b01724235849d4ff5baf
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2020-12-06 19:53:57 +0000

    removed concatenation plugin

--- aws/pluralsight-designingDeveloping/webpack.config.js
+++ aws/pluralsight-designingDeveloping/webpack.config.js
@@ -1,38 +1,35 @@
-const webpack = require('webpack')
-const path = require('path')
-const TerserPlugin = require('terser-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const webpack = require("webpack");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-  devtool: 'source-map',
-
   entry: {
-    application: './app/index.jsx'
+    application: "./app/index.jsx",
   },
 
-  mode: 'production',
+  mode: "production",
 
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties']
-        }
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: [
+            "@babel/plugin-proposal-object-rest-spread",
+            "@babel/plugin-proposal-class-properties",
+          ],
+        },
       },
       {
         test: /\.(less|css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader?modules',
-          'less-loader'
-        ]
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, "css-loader?modules", "less-loader"],
+      },
+    ],
   },
 
   optimization: {
@@ -41,17 +38,16 @@ module.exports = {
   },
 
   output: {
-    filename: '[name].min.js',
-    path: path.resolve(__dirname, 'public')
+    filename: "[name].min.js",
+    path: path.resolve(__dirname, "public"),
   },
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
     }),
-    new MiniCssExtractPlugin({ filename: 'stylesheet.css' }),
-    new webpack.optimize.ModuleConcatenationPlugin()
-  ]
-}
+    new MiniCssExtractPlugin({ filename: "stylesheet.css" }),
+  ],
+};
