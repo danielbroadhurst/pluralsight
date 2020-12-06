95eec9723fe31076f21575c18ef8038f661c5a0c
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2020-12-06 11:22:11 +0000

    Observer Pattern

--- javascript/pluralsight-designPatternsJS/behavioral/observer/main.js
+++ javascript/pluralsight-designPatternsJS/behavioral/observer/main.js
@@ -30,10 +30,75 @@ const auditingService = function () {
   };
 };
 
-let task1 = new Task({ name: "create a demo for constructors", user: "Jon" });
+// Observer list
+function ObserverList() {
+  this.observerList = [];
+}
+ObserverList.prototype.add = function (obj) {
+  return this.observerList.push(obj);
+};
+ObserverList.prototype.get = function (index) {
+  if (index > -1 && index < this.observerList.length) {
+    return this.observerList[index];
+  }
+};
+ObserverList.prototype.count = function () {
+  return this.observerList.length;
+};
+ObserverList.prototype.removeAt = function (index) {
+  this.observerList.splice(index, 1);
+};
+ObserverList.prototype.indexOf = function (obj, startIndex) {
+  let i = startIndex;
+  while (i < this.observerList.length) {
+    if (this.observerList[i] === obj) {
+      return i;
+    }
+    i++;
+  }
+  return -1;
+};
 
+// Wrap Task in an Observer
+const ObservableTask = function (data) {
+  Task.call(this, data);
+  this.observers = new ObserverList();
+};
+ObservableTask.prototype.addObserver = function (observer) {
+  this.observers.add(observer);
+};
+ObservableTask.prototype.notify = function (context) {
+  let observerCount = this.observers.count();
+  for (let i = 0; i < observerCount; i++) {
+    this.observers.get(i)(context);
+  }
+};
+ObservableTask.prototype.save = function () {
+  this.notify(this);
+  Task.prototype.save.call(this);
+};
+ObservableTask.prototype.removeObserver = function (observer) {
+  this.observers.removeAt(this.observers.indexOf(observer, 0));
+};
+
+// Create a new task
+let task1 = new ObservableTask({
+  name: "create a demo for constructors",
+  user: "Jon",
+});
+
+// Create Services
 let not = new notificationService();
 let ls = new loggingService();
 var audit = new auditingService();
 
+// Add the observers to the tast
+task1.addObserver(not.update);
+task1.addObserver(ls.update);
+task1.addObserver(audit.update);
+
+task1.save();
+// Remove a listener
+task1.removeObserver(audit.update);
+// Save again
 task1.save();
