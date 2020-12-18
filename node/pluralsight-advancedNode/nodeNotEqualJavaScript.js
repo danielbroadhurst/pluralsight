e8d10ae57352a1043f4244c4ecb4a412050ebcc4
G2S\daniel.broadhurst <daniel.broadhurst@matchstickmanhub.com>
2020-12-18 08:23:32 +0000

    node not equal to js

---
+++ node/pluralsight-advancedNode/nodeNotEqualJavaScript.js
@@ -0,0 +1,13 @@
+/**
+ * Node != Javascript
+ * - Node VM V8 - Comes with Feature Groups Shipping/Stage/In Progress
+ * - Node combines core modules, C++ Bindings, libuv and standard modules like http/fs
+ */
+
+// node - command would start a REPL
+//      - tab tab will trigger auto complete
+//      - require('repl') can create a repl session with options
+//      - node -p will evaluate a script
+//      - node -r will require a module
+
+// global - imported variables are not available 
