46da81e703579cbd78a18feb53c84329519d0f6c
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2021-01-09 08:18:31 +0000

    persistance in aws

--- aws/pluralsight-designingDeveloping/lib/data/lib/dynamo.js
+++ aws/pluralsight-designingDeveloping/lib/data/lib/dynamo.js
@@ -1,22 +1,40 @@
 const AWS = require('aws-sdk')
 
-AWS.config.update({ region: '/*TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 const client = new AWS.DynamoDB.DocumentClient()
 
 function getAll (tableName) {
-  // TODO: Declare params for scan
+  const params = {
+    TableName: tableName
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Scan table and return
+    client.scan(params, (err, data) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(data.Items)
+    })
   })
 }
 
 function get (tableName, id) {
-  // TODO: Declare params for query
+  const params = {
+    TableName: tableName,
+    KeyConditionExpression: 'id = :hkey',
+    ExpressionAttributeValues: {
+      ':hkey': +id
+    }
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Query table and return
+    client.query(params, (err, data) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(data.Items[0])
+    })
   })
 }
 
