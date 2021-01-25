1fea635bc5f362e8626889af98d6b4ef5a9e9bcd
G2S\daniel.broadhurst <daniel.broadhurst@matchstickmanhub.com>
2021-01-25 16:28:12 +0000

    module 3 - BRMs

---
+++ architecture/microservices/pluralsight-buildingReactiveMicroservices/document-service/target/generated-sources/annotations/com/pluralsight/reactive/document/mapper/DocumentMapperImpl.java
@@ -0,0 +1,42 @@
+package com.pluralsight.reactive.document.mapper;
+
+import com.pluralsight.reactive.document.model.domain.Document;
+import com.pluralsight.reactive.document.model.dto.DocumentDto;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-01-25T08:43:09+0000",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 1.3.1200.v20200916-0645, environment: Java 11.0.10 (AdoptOpenJDK)"
+)
+@Component
+public class DocumentMapperImpl implements DocumentMapper {
+
+    @Override
+    public DocumentDto asDto(Document document) {
+        if ( document == null ) {
+            return null;
+        }
+
+        DocumentDto documentDto = new DocumentDto();
+
+        return documentDto;
+    }
+
+    @Override
+    public Collection<DocumentDto> asDto(Collection<Document> documents) {
+        if ( documents == null ) {
+            return null;
+        }
+
+        Collection<DocumentDto> collection = new ArrayList<DocumentDto>( documents.size() );
+        for ( Document document : documents ) {
+            collection.add( asDto( document ) );
+        }
+
+        return collection;
+    }
+}
---
+++ architecture/microservices/pluralsight-buildingReactiveMicroservices/translation-service/target/generated-sources/annotations/com/pluralsight/reactive/translation/mapper/SubmissionMapperImpl.java
@@ -0,0 +1,42 @@
+package com.pluralsight.reactive.translation.mapper;
+
+import com.pluralsight.reactive.translation.model.domain.Submission;
+import com.pluralsight.reactive.translation.model.dto.SubmissionDto;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-01-25T08:43:13+0000",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 1.3.1200.v20200916-0645, environment: Java 11.0.10 (AdoptOpenJDK)"
+)
+@Component
+public class SubmissionMapperImpl implements SubmissionMapper {
+
+    @Override
+    public SubmissionDto asDto(Submission submission) {
+        if ( submission == null ) {
+            return null;
+        }
+
+        SubmissionDto submissionDto = new SubmissionDto();
+
+        return submissionDto;
+    }
+
+    @Override
+    public Collection<SubmissionDto> asDto(Collection<Submission> submissions) {
+        if ( submissions == null ) {
+            return null;
+        }
+
+        Collection<SubmissionDto> collection = new ArrayList<SubmissionDto>( submissions.size() );
+        for ( Submission submission : submissions ) {
+            collection.add( asDto( submission ) );
+        }
+
+        return collection;
+    }
+}
