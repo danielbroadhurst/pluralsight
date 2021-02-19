086feec93cf0309513fb8e7f0144b516b894e84f
G2S\daniel.broadhurst <daniel.broadhurst@matchstickmanhub.com>
2021-02-19 08:25:38 +0000

    lambda deep dive

---
+++ aws/pluralsight-lambdaDeepDive/m3-writingAndDeployingLambda/lambda_function.py
@@ -0,0 +1,14 @@
+import boto3
+import logging
+
+client = boto3.client('dynamodb')
+logger = logging.getLogger();
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    logger.info('Function:{} has started'.format(context.function_name))
+    print('this is a simple log')
+    client.create_backup(
+        TableName='inventory',
+        BackupName='MyAutomaticBackup'
+        )
