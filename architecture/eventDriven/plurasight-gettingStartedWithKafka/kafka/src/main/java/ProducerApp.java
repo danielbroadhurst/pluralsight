a5b37006a60bae5f55afb06acc4c695be0df7374
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2021-05-10 14:02:14 +0100

    kafka introduction

---
+++ architecture/eventDriven/plurasight-gettingStartedWithKafka/kafka/src/main/java/ProducerApp.java
@@ -0,0 +1,37 @@
+import org.apache.kafka.clients.producer.*;
+
+import java.text.*;
+import java.util.*;
+
+public class ProducerApp {
+
+    public static void main(String[] args){
+
+        // Create the Properties class to instantiate the Consumer with the desired settings:
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("acks", "1");
+
+
+        KafkaProducer<String, String> myProducer = new KafkaProducer<String, String>(props);
+        DateFormat dtFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS");
+        String topic = "my_topic";
+
+        int numberOfRecords = 100; // number of records to send
+        long sleepTimer = 0; // how long you want to wait before the next record to be sent
+
+        try {
+                for (int i = 0; i < numberOfRecords; i++ )
+                    myProducer.send(new ProducerRecord<String, String>(topic, String.format("Message: %s  sent at %s", Integer.toString(i), dtFormat.format(new Date()))));
+                    Thread.sleep(sleepTimer);
+                    // Thread.sleep(new Random(5000).nextLong()); // use if you want to randomize the time between record sends
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            myProducer.close();
+        }
+
+    }
+}
